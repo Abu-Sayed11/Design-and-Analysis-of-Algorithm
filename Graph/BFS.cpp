480dde1093842856c3c80e822469eab505bbdfc8
abu sayed <81965394+Abu-Sayed11@users.noreply.github.com>
2023-02-02 21:13:33 +0600

    Graph traversal using BFS algorithm.

---
+++ Graph/BFS.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void BFS(vector<int>Graph[],int n,int vertex){
+    bool visited[vertex];
+    visited[n] = true;
+    queue<int>Q;
+    Q.push(n);
+    cout << "Graph treversing with BFS : ";
+    while(!Q.empty()){
+        n = Q.front();
+        Q.pop();
+        cout << n << " ";
+        for(int i = 0; i < Graph[n].size(); i++){
+            if(!visited[Graph[n][i]]){
+                visited[Graph[n][i]] = true;
+                Q.push(Graph[n][i]);
+            }
+        }
+    }
+}
+
+auto create_graph(vector<int>Graph[],int edge){
+    for(int i = 0; i < edge; i++){
+        int u,v; cin >> u >> v;
+        Graph[u].push_back(v);
+    }
+    return Graph;
+}
+
+int main(){
+
+    int vertex,edge,src;
+
+    cout << "Enter vertex : ";
+    cin >> vertex ;
+
+    cout << "Enter edge : ";
+    cin >> edge ;
+
+    vector<int> Graph[vertex];
+    
+    auto g = create_graph(Graph,edge);
+
+    cout << "Enter source : ";
+    cin >> src;
+
+    BFS(g,src,vertex);
+
+    return 0;
+}
